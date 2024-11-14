732a266ab6d8f0d481223f19952faab8d3e69e66
joseph skokan <joeskokan20@gmail.com>
2024-11-14 18:48:20 -0500

    sermon controllers

--- routes/sermonRoutes.js
+++ routes/sermonRoutes.js
@@ -4,4 +4,5 @@ const SermonController = require("../controllers/SermonController");
 
 router.post("/sermons", SermonController.createSermon);
 router.get("/sermons", SermonController.findAllSermons);
+router.delete("/sermons/:id", SermonController.deleteSermon);
 module.exports = router;
