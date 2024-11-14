55d0b4520550abacbf83ae6cab7676fa61bd8959
joseph skokan <joeskokan20@gmail.com>
2024-11-14 13:44:31 -0500

    event model fix

--- models/Event.js
+++ models/Event.js
@@ -1,18 +1,22 @@
 const { sequelize, DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const Event = sequelize.define("Event", {
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
+const Event = sequelize.define(
+  "Event",
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
   },
-  date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-  },
-});
-
-module.exports = Event;
+  {
+    timestamps: true, // this will handle the timestamps
+  }
+);
